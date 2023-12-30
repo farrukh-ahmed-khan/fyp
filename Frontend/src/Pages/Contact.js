6cc54e8129b73f02230d518cfae374d037062f8d
Muhammad Sarim Khan Bangash <75114586+m-sarim-khan-bangash@users.noreply.github.com>
2023-12-31 02:16:43 +0500

    some changes

--- Frontend/src/Pages/Contact.js
+++ Frontend/src/Pages/Contact.js
@@ -1,53 +1,81 @@
-import { React, useState } from 'react';
-import '../Assets/css/contact.css';
-import fbIcon from '../Assets/images/facebook-icon.png';
-import instaIcon from '../Assets/images/instagram-icon.png';
-import twitterIcon from '../Assets/images/whatsapp-icon.png';
-import whatsappIcon from '../Assets/images/twitter-icon.png';
-import Navbar from '../Components/CommonComponent/Nav';
-import Footer from '../Components/CommonComponent/Footer';
+import { React, useState } from "react";
+import "../Assets/css/contact.css";
+import fbIcon from "../Assets/images/facebook-icon.png";
+import instaIcon from "../Assets/images/instagram-icon.png";
+import twitterIcon from "../Assets/images/whatsapp-icon.png";
+import whatsappIcon from "../Assets/images/twitter-icon.png";
+import img from "../Assets/images/contact.png";
+import Navbar from "../Components/CommonComponent/Nav";
+import Footer from "../Components/CommonComponent/Footer";
 import { Link, useNavigate } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 const Contact = () => {
-    const [values, setValues] = useState({
-      name: '',
-      phone: '',
-      email: '',
-      message: '',
-    });
-const navigate = useNavigate();
-  
-    const handleInput = (e) => {
-      setValues((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-    };
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-  
-      const { name, phone, email, message } = values;
-  
-      if (email && phone && name && message) {
-        axios
-          .post('http://localhost:8081/contact', values)
-          .then((res) => {
-            if (!res.data.error) {
-              navigate('/Thankyou');
-            } 
-          })
-          .catch((err) => {
-            console.error(err);
-            alert('Data not sent');
-          });
-      }
-    };
-  
+  const [values, setValues] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    message: "",
+  });
+  const navigate = useNavigate();
+
+  const handleInput = (e) => {
+    setValues((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { name, phone, email, message } = values;
+
+    if (email && phone && name && message) {
+      axios
+        .post("http://localhost:8081/contact", values)
+        .then((res) => {
+          if (!res.data.error) {
+            navigate("/Thankyou");
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+          alert("Data not sent");
+        });
+    }
+  };
+
   return (
     <>
       <Navbar />
-      {/* Hero Section */}
-      <div class="banner-section">
-        <h1>Welcome To Contact Page</h1>
+      <div className="container">
+        <div className="service-wrapper">
+          <div className="row">
+            <div className="col-lg-6 mt-5">
+              <h3>
+                <span>| </span>CONTACT
+              </h3>
+              <p>
+                Welcome to the wedding spot, where we don't just provide spaces
+                but create an entire experience. Elevate your event with our
+                comprehensive services. Our talented photographers skillfully
+                capture every fleeting moment, ensuring memories that last a
+                lifetime. Transform into your most radiant self with our expert
+                makeup artists, who bring out your natural beauty. Let our
+                decorators weave magic, turning your chosen venue into a
+                personalized masterpiece. Together, these services harmonize to
+                craft an unforgettable celebration, where every detail is a
+                testament to your unique style and joyous moments are
+                immortalized with grace.
+              </p>
+            </div>
+            <div className="col-lg-6 ml-1">
+              <img src={img} alt="" />
+            </div>
+          </div>
+        </div>
       </div>
+      {/* Hero Section */}
+      {/* <div class="banner-section">
+        <h1>Welcome To Contact Page</h1>
+      </div> */}
       <h1 className="text-center contact-heading">Contact US</h1>
       <div className="contact-us">
         <center>
@@ -84,7 +112,7 @@ const navigate = useNavigate();
                 cols={70}
                 rows={8}
                 placeholder="Type Your Messege Here...."
-                defaultValue={''}
+                defaultValue={""}
                 className="messege-field"
                 name="message"
                 onChange={handleInput}
