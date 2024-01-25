556576e9e025a986f1d17d93874ad352bd9a460b
Muhammad Sarim Khan Bangash <75114586+m-sarim-khan-bangash@users.noreply.github.com>
2024-01-25 09:28:58 +0500

    some-changes

--- Frontend/src/AdminPanel/AdminService.js
+++ Frontend/src/AdminPanel/AdminService.js
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import '../Assets/css/admin.css';
-import DashboardImg from '../Assets/images/AdminPanel/dashboard.png';
-import ServiceImg from '../Assets/images/AdminPanel/service.png';
-import MsgImg from '../Assets/images/AdminPanel/messages.png';
-import PaymentImg from '../Assets/images/AdminPanel/payment.png';
-import ViewListImg from '../Assets/images/AdminPanel/view-list.png';
-import user from '../Assets/images/AdminPanel/user.png';
-import { Link } from 'react-router-dom/dist/umd/react-router-dom.development';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import "../Assets/css/admin.css";
+import DashboardImg from "../Assets/images/AdminPanel/dashboard.png";
+import ServiceImg from "../Assets/images/AdminPanel/service.png";
+import MsgImg from "../Assets/images/AdminPanel/messages.png";
+import PaymentImg from "../Assets/images/AdminPanel/payment.png";
+import ViewListImg from "../Assets/images/AdminPanel/view-list.png";
+import user from "../Assets/images/AdminPanel/user.png";
+import { Link } from "react-router-dom/dist/umd/react-router-dom.development";
+import axios from "axios";
 
 const AdminService = () => {
-  const [service, setService] = useState('');
-  const [packageType, setPackageType] = useState('');
-  const [price, setPrice] = useState('');
+  const [service, setService] = useState("");
+  const [packageType, setPackageType] = useState("");
+  const [price, setPrice] = useState("");
 
   const handleEditPrice = async () => {
     try {
       // Send a POST request to the backend API
-      const response = await axios.put('http://localhost:8081/update-prices', {
+      const response = await axios.put("http://localhost:8081/update-prices", {
         service,
         packageType,
         price,
@@ -25,11 +25,11 @@ const AdminService = () => {
 
       if (response.data.success) {
         // Handle success (you can show a success message or update the UI)
-        console.log('Price edited successfully!');
+        console.log("Price edited successfully!");
       }
     } catch (error) {
       // Handle error (show an error message or log the error)
-      console.error('Error editing price:', error);
+      console.error("Error editing price:", error);
     }
   };
 
@@ -88,37 +88,47 @@ const AdminService = () => {
               </ul>
             </div>
           </div>
-          <div className='details'>
-            <h1>Edit Service Prices</h1>
-            <label>
-              Service:
-              <input
-                type="text"
-                value={service}
-                onChange={(e) => setService(e.target.value)}
-              />
-            </label>
-            <br />
-            <label>
-              Package:
-              <input
-                type="text"
-                value={packageType}
-                onChange={(e) => setPackageType(e.target.value)}
-              />
-            </label>
-            <br />
-            <label>
-              Price:
-              <input
-                type="text"
-                value={price}
-                onChange={(e) => setPrice(e.target.value)}
-              />
-            </label>
-            <br />
-            <button onClick={handleEditPrice}>Edit Price</button>
-          </div>
+          <center>
+            <div className="details p-3">
+              <h1 style={{ padding: "10px 10px" }}>Edit Service Prices</h1>
+              <label>
+                Service
+                <br />
+                <input
+                  className="form-control"
+                  type="text"
+                  value={service}
+                  onChange={(e) => setService(e.target.value)}
+                />
+              </label>
+              <br />
+              <label>
+                Package
+                <br />
+                <input
+                  className="form-control"
+                  type="text"
+                  value={packageType}
+                  onChange={(e) => setPackageType(e.target.value)}
+                />
+              </label>
+              <br />
+              <label>
+                Price
+                <br />
+                <input
+                  className="form-control"
+                  type="text"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </label>
+              <br />
+              <button className="btn " onClick={handleEditPrice}>
+                Edit Price
+              </button>
+            </div>
+          </center>
         </div>
       </div>
     </>
